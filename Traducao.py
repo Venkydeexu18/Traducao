2df42006150b27150a615b42a972d8d54bc3c8eb
Venkydeexu18 <deekshitkandregula@gmail.com>
2022-11-09 14:50:05 +0530

    18

--- Traducao.py
+++ Traducao.py
@@ -176,4 +176,4 @@ def index_contact():
 def index_abt():
     return render_template('about_us.html')
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=80)\
